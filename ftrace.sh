8a06365f92010f8569a4fd80dd03eb08e77a5ae6
kang-hyuck <kang-hyuck@naver.com>
2025-08-08 17:36:20 +0900

    scripts: update commands

--- ftrace.sh
+++ ftrace.sh
@@ -2,15 +2,27 @@
 ############## function definitions ##############
 function print_usage(){
     echo "usage:"
+    echo "  - tracing run:"
+    echo "       $0 run <functions>"
     echo "  - tracing on:"
-    echo "       $0 on <functions>"
+    echo "       $0 on"
     echo "  - tracing off:"
     echo "       $0 off"
     echo "  - tracing off & copy data:"
     echo "       $0 g"
+    echo "  - set first filter function"
+    echo "       $0 set <functions>"
+    echo "  - add filter function"
+    echo "       $0 add <functions>"
+    echo "  - grep available filter function"
+    echo "       $0 grep <functions>"
     echo " "
     echo "functions:"
     echo "  - show_interrupts"
+    echo "  - __x64_sys_clone"
+    echo "  - do_exit"
+    echo "  - __do_sys_fork"
+    echo "  - copy_process"
     echo " "
 }
 
@@ -21,6 +33,14 @@ function config_event(){
         echo 1 > /sys/kernel/debug/tracing/events/irq/irq_handler_exit/enable
         sleep 1
         echo "event enabled"
+
+    elif [ "$FUNCTIONS" =  "__x64_sys_clone" ]; then
+        echo 1 > /sys/kernel/debug/tracing/events/sched/sched_switch/enable
+        echo 1 > /sys/kernel/debug/tracing/events/sched/sched_wakeup/enable
+        echo 1 > /sys/kernel/debug/tracing/events/sched/sched_process_fork/enable
+        echo 1 > /sys/kernel/debug/tracing/events/sched/sched_process_exit/enable
+        sleep 1
+        echo "event enabled"
     fi
 }
 
@@ -28,7 +48,7 @@ function config_event(){
 COMMAND=$1
 FUNCTIONS=$2
 
-if [ "$COMMAND" = "on" ]; then
+if [ "$COMMAND" = "run" ]; then
     ## check - args
     if [ $# -lt 2 ]; then
         print_usage
@@ -82,7 +102,83 @@ elif [ "$COMMAND" = "g" ]; then
 elif [ "$COMMAND" = "off" ]; then
     ## tracing off
     echo 0 > /sys/kernel/debug/tracing/tracing_on
+
+    ## event disabled
+    echo 0 > /sys/kernel/debug/tracing/events/enable
+    sleep 1
+
     echo "tracing off"
+
+elif [ "$COMMAND" = "add" ]; then
+    ## check - args
+    if [ $# -lt 2 ]; then
+        print_usage
+        exit 1
+    fi
+
+    ## check - availability of tracing functions
+    if grep -xq "$FUNCTIONS" /sys/kernel/debug/tracing/available_filter_functions; then
+        echo "$FUNCTIONS: available"
+    else
+        echo "$FUNCTIONS: not available"
+        exit 1
+    fi
+
+    ## config event
+    config_event
+
+    ## add functions (append: >>)
+    echo $FUNCTIONS >> /sys/kernel/debug/tracing/set_ftrace_filter
+    sleep 1
+
+    echo "add '$FUNCTIONS': ok"
+
+elif [ "$COMMAND" = "set" ]; then
+    ## check - args
+    if [ $# -lt 2 ]; then
+        print_usage
+        exit 1
+    fi
+
+    ## check - availability of tracing functions
+    if grep -xq "$FUNCTIONS" /sys/kernel/debug/tracing/available_filter_functions; then
+        echo "$FUNCTIONS: available"
+    else
+        echo "$FUNCTIONS: not available"
+        exit 1
+    fi
+
+    ## config event
+    config_event
+
+    ## set functions
+    echo $FUNCTIONS > /sys/kernel/debug/tracing/set_ftrace_filter
+    sleep 1
+
+    echo "set '$FUNCTIONS': ok"
+
+elif [ "$COMMAND" = "grep" ]; then
+    ## check - args
+    if [ $# -lt 2 ]; then
+        print_usage
+        exit 1
+    fi
+
+    ## print grep reults
+    grep "$FUNCTIONS" /sys/kernel/debug/tracing/available_filter_functions
+
+elif [ "$COMMAND" = "on" ]; then
+    ## select tracer (function)
+    echo function > /sys/kernel/debug/tracing/current_tracer
+    sleep 1
+
+    ## config function stack trace
+    echo 1 > /sys/kernel/debug/tracing/options/func_stack_trace
+    echo 1 > /sys/kernel/debug/tracing/options/sym-offset
+
+    ## tracing on
+    echo 1 > /sys/kernel/debug/tracing/tracing_on
+    echo "tracing on"
 
 else
     print_usage
